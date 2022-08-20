e982fe2c23b58707219b294ab565ffd97b7aafd9
masomdewan24 <masom24@gmail.com>
2022-08-20 08:50:54 +0600

    final

--- js/app.js
+++ js/app.js
@@ -21,4 +21,50 @@ displyPin.value = pin;
 
 
 });
+document.getElementById('calculator'),addEventListener('click', function(event){
+    
+    const number = event.target.innerText;
+    const typeNumberField = document.getElementById('typeNumber');
+    const previusTypeNumber = typeNumberField.value;
+    if(isNaN(number)){
+        
+        if(number === 'C'){
+            typeNumberField.value = '';
 
+        }
+        else if(number === '<'){
+            const digits = previusTypeNumber.split('');
+            digits.pop();
+            const reminingDigits = digits.join('');
+            typeNumberField.value = reminingDigits;
+
+        }
+    }
+    else{
+        
+        
+        const newTypeNumber = previusTypeNumber + number;
+        typeNumberField.value = newTypeNumber;
+    }
+
+});
+document.getElementById('verify-pin').addEventListener('click', function(){
+    const displyPinNumber = document.getElementById('disply-pin');
+    const curentNumber = displyPinNumber.value;
+
+    const typeNumber = document.getElementById('typeNumber');
+    const curentTypeNumber = typeNumber.value;
+
+    const pinSucess = document.getElementById('pin-sucess');
+    const painFailMsg = document.getElementById('pin-failur');
+
+    if(curentNumber == curentTypeNumber){
+       pinSucess.style.display = 'block';
+       painFailMsg.style.display = 'none';
+    }
+    else{
+       
+        painFailMsg.style.display = 'block';
+        pinSucess.style.display = 'none';
+    }
+})
